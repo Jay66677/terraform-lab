1571797be189040b710c80eb1708686dcd3c9aa2
Jay66677 <jayasaibadigeru@gmail.com>
2026-03-23 00:58:58 +0530

    Bonus assignment dynamic containers completed

---
+++ day5/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+variable "container_config" {
+  type = list(object({
+    name = string
+    port = number
+  }))
+}
+
+module "nginx_containers" {
+  source = "../day4/modules/nginx_container"
+
+  for_each = {
+    for container in var.container_config :
+    container.name => container
+  }
+
+  container_name = each.value.name
+  container_port = each.value.port
+  image_name     = "nginx:latest"
+}
+
+output "container_urls" {
+  value = [
+    for c in var.container_config :
+    "http://localhost:${c.port}"
+  ]
+}
